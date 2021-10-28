06ab79954209a3fedadd1436c6d1bbffd0360657
Tommi Alajoki <tommi@alajoki.fi>
2021-10-28 18:39:11 +0300

    Added comments

--- index.php
+++ index.php
@@ -1,41 +1,78 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/darkly/bootstrap.min.css" crossorigin="anonymous">
+</head>
+<body>
+</body>
+</html>
+
 <?php
 
 //decode the json into an object
-$data = file_get_contents("./sales_data_big.json");
+$data = file_get_contents("./sales_data.json");
 $json_data = json_decode($data);
 
-//initializing total vars and array for products
+/*
+* json is an array of orders with the following structure
+*
+* order:
+*   {
+*       id: int,
+*       language: string,
+*       customer_name: string,
+*       country: string
+*   },
+* lines: 
+*   [...,{
+*       id: int,
+*       vat_percentage: int,
+*       unit_price_without_vat: int,
+*       product: string
+*   },...]
+*
+* with the lines array being any length
+*/
+
+//initializing sum vars and array of counted products (to be used for uniques)
 $vat_total = 0;
 $discount_total = 0;
 $discount_with_vat_total = 0;
 $products = array();
 
+//loop through orders
 foreach($json_data as $datapoint){
-    echo "Order id: {$datapoint->order->id}, by: {$datapoint->order->customer_name} <br>";
+    
+    //loop through the lines array of an order
     foreach($datapoint->lines as $line){
+
+        //amount of vat paid for one product
         $vat = $line->unit_price_without_vat * ($line->vat_percentage/100);
+        
+        //add to total sum (times quantity of product)
         $vat_total += $vat * $line->qty;
-        $vat_price = $line->unit_price_without_vat * (1 + $line->vat_percentage/100);
 
         $discount = $line->unit_price_without_vat * ($line->discount_percentage/100);
         $discount_added_vat = $discount * (1 + $line->vat_percentage/100);
 
-        $discount_total += $discount;
-        $discount_with_vat_total += $discount_added_vat;
+        $discount_total += $discount * $line->qty;
+        $discount_with_vat_total += $discount_added_vat * $line->qty;
 
+        //add product name to array
         array_push($products, $line->product);
 
-        echo "{$line->qty} x {$line->product} - VAT free price: {$line->unit_price_without_vat}, VAT included: {$vat_price}, VAT: {$line->vat_percentage}% <br>";
     }
 }
 
-
+//remove duplicates from array and get it's size
 $size = sizeof(array_unique($products));
 
-echo "Total VAT to be paid: {$vat_total} € <br>";
-echo "Total discount: {$discount_total} € (VAT 0%), {$discount_with_vat_total} € (VAT included) <br>";
-echo "Total amount of unique products: {$size}";
-
-
+//echo a table for visuals
+echo "<table class='table'><tbody>";
+echo "<tr><td>Total VAT to be paid</td><td> {$vat_total} €</td> </tr>";
+echo "<tr><td>Total discount given</td><td> {$discount_total} € (VAT 0%), {$discount_with_vat_total} € (VAT included)</td>  </tr>";
+echo "<tr><td>Total amount of unique products</td><td> {$size}</td> </tr>";
+echo "</tbody></table>";
 
 ?>
+
+
